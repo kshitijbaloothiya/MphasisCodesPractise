75a26f360444b7dae6b3c62818ebf670ee654ef2
Kshitij Baloothiya <kshitijbaloothiya@gmail.com>
2021-07-26 12:45:19 +0530

    containing threadpool and logger

---
+++ MiniProj/src/com/mphasis/cui/ClientGatewayExec.java
@@ -0,0 +1,11 @@
+package com.mphasis.cui;
+
+public class ClientGatewayExec implements Runnable {
+
+    //This class will represent the LOCAL client exec feed.
+
+    public void run(){
+
+    }
+
+}
---
+++ MiniProj/src/com/mphasis/cui/Message.java
@@ -0,0 +1,13 @@
+package com.mphasis.cui;
+
+public class Message {
+
+    private boolean isOrder;
+    private String clientID;
+
+    private String orderID;
+
+
+
+
+}
